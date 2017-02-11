3564a5f9c857bc4fff9eef2f4be271172bb68463
Cooper Maruyama <cooperm@susteen.com>
2017-02-10 17:25:27 -0800

    Support running locally if you don't want to use docker.

--- settings.js
+++ settings.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 module.exports = {
     APP_ROOT: __dirname,
     APP_PORT: process.env.PORT || 3000,
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://mongo:27017/app'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/app'
 };
